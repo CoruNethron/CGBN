3900d9f00f0e7f1ea01cd7383960cc041cc81542
Denis Golovkin <d.golovkin@baum.ru>
2024-11-18 11:01:21 +1000

    use c++11, compress comments, whitespace, wip ck 256 bit sub

--- samples/sample_01_add/add.cu
+++ samples/sample_01_add/add.cu
@@ -1,28 +1,3 @@
-/***
-
-Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-***/
-
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -33,29 +8,6 @@ IN THE SOFTWARE.
 #include "../utility/cpu_simple_bn_math.h"
 #include "../utility/gpu_support.h"
 
-/************************************************************************************************
- *  This example performs component-wise addition of two arrays of 1024-bit bignums.
- *
- *  The example uses a number of utility functions and macros:
- *
- *    random_words(uint32_t *words, uint32_t count)
- *       fills words[0 .. count-1] with random data
- *
- *    add_words(uint32_t *r, uint32_t *a, uint32_t *b, uint32_t count) 
- *       sets bignums r = a+b, where r, a, and b are count words in length
- *
- *    compare_words(uint32_t *a, uint32_t *b, uint32_t count)
- *       compare bignums a and b, where a and b are count words in length.
- *       return 1 if a>b, 0 if a==b, and -1 if b>a
- *    
- *    CUDA_CHECK(call) is a macro that checks a CUDA result for an error,
- *    if an error is present, it prints out the error, call, file and line.
- *
- *    CGBN_CHECK(report) is a macro that checks if a CGBN error has occurred.
- *    if so, it prints out the error, and instance information
- *
- ************************************************************************************************/
- 
 // IMPORTANT:  DO NOT DEFINE TPI OR BITS BEFORE INCLUDING CGBN
 #define TPI 32
 #define BITS 1024
--- samples/sample_03_powm/powm_odd.cu
+++ samples/sample_03_powm/powm_odd.cu
@@ -1,27 +1,3 @@
-/***
-
-Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-
-***/
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -30,42 +6,26 @@ IN THE SOFTWARE.
 #include "cgbn/cgbn.h"
 #include "../utility/support.h"
 
-// For this example, there are quite a few template parameters that are used to generate the actual code.
-// In order to simplify passing many parameters, we use the same approach as the CGBN library, which is to
-// create a container class with static constants and then pass the class.
-
-// The CGBN context uses the following three parameters:
-//   TBP             - threads per block (zero means to use the blockDim.x)
-//   MAX_ROTATION    - must be small power of 2, imperically, 4 works well
-//   SHM_LIMIT       - number of bytes of dynamic shared memory available to the kernel
-//   CONSTANT_TIME   - require constant time algorithms (currently, constant time algorithms are not available)
-
-// Locally it will also be helpful to have several parameters:
-//   TPI             - threads per instance
-//   BITS            - number of bits per instance
-//   WINDOW_BITS     - number of bits to use for the windowed exponentiation
-
 template<uint32_t tpi, uint32_t bits, uint32_t window_bits>
 class powm_params_t {
   public:
   // parameters used by the CGBN context
-  static const uint32_t TPB=0;                     // get TPB from blockDim.x  
-  static const uint32_t MAX_ROTATION=4;            // good default value
-  static const uint32_t SHM_LIMIT=0;               // no shared mem available
+  static const uint32_t TPB=0;                     // threads per block (zero means to use the blockDim.x)
+  static const uint32_t MAX_ROTATION=4;            // must be small power of 2, imperically, 4 works well
+  static const uint32_t SHM_LIMIT=0;               // number of bytes of dynamic shared memory available to the kernel
   static const bool     CONSTANT_TIME=false;       // constant time implementations aren't available yet
-  
+
   // parameters used locally in the application
   static const uint32_t TPI=tpi;                   // threads per instance
-  static const uint32_t BITS=bits;                 // instance size
-  static const uint32_t WINDOW_BITS=window_bits;   // window size
+  static const uint32_t BITS=bits;                 // number of bits per instance
+  static const uint32_t WINDOW_BITS=window_bits;   // number of bits to use for the windowed exponentiation
 };
 
 template<class params>
 class powm_odd_t {
   public:
   static const uint32_t window_bits=params::WINDOW_BITS;  // used a lot, give it an instance variable
-  
-  // define the instance structure
+
   typedef struct {
     cgbn_mem_t<params::BITS> x;
     cgbn_mem_t<params::BITS> power;
@@ -82,8 +42,8 @@ class powm_odd_t {
   env_t     _env;
   int32_t   _instance;
   
-  __device__ __forceinline__ powm_odd_t(cgbn_monitor_t monitor, cgbn_error_report_t *report, int32_t instance) : _context(monitor, report, (uint32_t)instance), _env(_context), _instance(instance) {
-  }
+  __device__ __forceinline__ powm_odd_t(cgbn_monitor_t monitor, cgbn_error_report_t *report, int32_t instance) :
+    _context(monitor, report, (uint32_t)instance), _env(_context), _instance(instance) {}
 
   __device__ __forceinline__ void fixed_window_powm_odd(bn_t &result, const bn_t &x, const bn_t &power, const bn_t &modulus) {
     bn_t       t;
@@ -91,18 +51,17 @@ class powm_odd_t {
     int32_t    index, position, offset;
     uint32_t   np0;
 
-    // conmpute x^power mod modulus, using the fixed window algorithm
-    // requires:  x<modulus,  modulus is odd
+    // conmpute x^power mod modulus, using the fixed window algorithm, requires:  x<modulus,  modulus is odd
 
     // compute x^0 (in Montgomery space, this is just 2^BITS - modulus)
     cgbn_negate(_env, t, modulus);
     cgbn_store(_env, window+0, t);
-    
+
     // convert x into Montgomery space, store into window table
     np0=cgbn_bn2mont(_env, result, x, modulus);
     cgbn_store(_env, window+1, result);
     cgbn_set(_env, t, result);
-    
+
     // compute x^2, x^3, ... x^(2^window_bits-1), store into window table
     #pragma nounroll
     for(index=2;index<(1<<window_bits);index++) {
@@ -110,8 +69,7 @@ class powm_odd_t {
       cgbn_store(_env, window+index, result);
     }
 
-    // find leading high bit
-    position=params::BITS - cgbn_clz(_env, power);
+    position=params::BITS - cgbn_clz(_env, power); // find leading high bit
 
     // break the exponent into chunks, each window_bits in length
     // load the most significant non-zero exponent chunk
@@ -122,23 +80,19 @@ class powm_odd_t {
       position=position-offset;
     index=cgbn_extract_bits_ui32(_env, power, position, window_bits);
     cgbn_load(_env, result, window+index);
-    
-    // process the remaining exponent chunks
-    while(position>0) {
-      // square the result window_bits times
+
+    while(position>0) { // process the remaining exponent chunks
       #pragma nounroll
       for(int sqr_count=0;sqr_count<window_bits;sqr_count++)
         cgbn_mont_sqr(_env, result, result, modulus, np0);
-      
-      // multiply by next exponent chunk
+
       position=position-window_bits;
       index=cgbn_extract_bits_ui32(_env, power, position, window_bits);
       cgbn_load(_env, t, window+index);
       cgbn_mont_mul(_env, result, result, t, modulus, np0);
     }
-    
-    // we've processed the exponent now, convert back to normal space
-    cgbn_mont2bn(_env, result, result, modulus, np0);
+
+    cgbn_mont2bn(_env, result, result, modulus, np0); // we've processed the exponent now, convert back to normal space
   }
   
   __device__ __forceinline__ void sliding_window_powm_odd(bn_t &result, const bn_t &x, const bn_t &power, const bn_t &modulus) {
@@ -147,17 +101,14 @@ class powm_odd_t {
     uint32_t     mont_inv;
     bn_local_t   odd_powers[1<<window_bits-1];
 
-    // conmpute x^power mod modulus, using Constant Length Non-Zero windows (CLNZ).
-    // requires:  x<modulus,  modulus is odd
+    // conmpute x^power mod modulus, using Constant Length Non-Zero windows (CLNZ), requires:  x<modulus,  modulus is odd
         
     // find the leading one in the power
     leading=params::BITS-1-cgbn_clz(_env, power);
     if(leading>=0) {
-      // convert x into Montgomery space, store in the odd powers table
-      mont_inv=cgbn_bn2mont(_env, result, x, modulus);
-      
-      // compute t=x^2 mod modulus
-      cgbn_mont_sqr(_env, t, result, modulus, mont_inv);
+      mont_inv=cgbn_bn2mont(_env, result, x, modulus); // convert x into Montgomery space, store in the odd powers table
+
+      cgbn_mont_sqr(_env, t, result, modulus, mont_inv); // compute t=x^2 mod modulus
       
       // compute odd powers window table: x^1, x^3, x^5, ...
       cgbn_store(_env, odd_powers, result);
@@ -166,9 +117,8 @@ class powm_odd_t {
         cgbn_mont_mul(_env, result, result, t, modulus, mont_inv);
         cgbn_store(_env, odd_powers+index, result);
       }
-  
-      // starts contains an array of bits indicating the start of a window
-      cgbn_set_ui32(_env, starts, 0);
+
+      cgbn_set_ui32(_env, starts, 0); // starts contains an array of bits indicating the start of a window
   
       // organize p as a sequence of odd window indexes
       position=0;
@@ -189,29 +139,35 @@ class powm_odd_t {
       index=cgbn_extract_bits_ui32(_env, power, position+1, window_bits-1);
       cgbn_load(_env, result, odd_powers+index);
       position--;
-      
-      // Process remaining windows 
-      while(position>=0) {
+
+      while(position>=0) { // Process remaining windows
         cgbn_mont_sqr(_env, result, result, modulus, mont_inv);
-        if(cgbn_extract_bits_ui32(_env, starts, position, 1)==1) {
-          // found a window, load the index
+        if(cgbn_extract_bits_ui32(_env, starts, position, 1)==1) { // found a window, load the index
           index=cgbn_extract_bits_ui32(_env, power, position+1, window_bits-1);
           cgbn_load(_env, t, odd_powers+index);
           cgbn_mont_mul(_env, result, result, t, modulus, mont_inv);
         }
         position--;
       }
-      
-      // convert result from Montgomery space
-      cgbn_mont2bn(_env, result, result, modulus, mont_inv);
+
+      cgbn_mont2bn(_env, result, result, modulus, mont_inv); // convert result from Montgomery space
     }
-    else {
-      // p=0, thus x^p mod modulus=1
-      cgbn_set_ui32(_env, result, 1);
-    }
+    else cgbn_set_ui32(_env, result, 1); // p=0, thus x^p mod modulus=1
   }
   
   __host__ static instance_t *generate_instances(uint32_t count) {
+      static cgbn_mem_t<params::BITS> tmp;
+
+      mpz_t p, gx, gy;
+
+      set_words(tmp._limbs, "fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f", params::BITS/32);
+
+      mpz_init(p);
+      mpz_init(gx);
+      mpz_init(gy);
+
+      to_mpz(p, tmp._limbs, params::BITS/32);
+
     instance_t *instances=(instance_t *)malloc(sizeof(instance_t)*count);
     int         index;
   
@@ -220,19 +176,16 @@ class powm_odd_t {
       random_words(instances[index].power._limbs, params::BITS/32);
       random_words(instances[index].modulus._limbs, params::BITS/32);
 
-      // ensure modulus is odd
-      instances[index].modulus._limbs[0] |= 1;
+      instances[index].modulus._limbs[0] |= 1; // ensure modulus is odd
 
       // ensure modulus is greater than 
       if(compare_words(instances[index].x._limbs, instances[index].modulus._limbs, params::BITS/32)>0) {
         swap_words(instances[index].x._limbs, instances[index].modulus._limbs, params::BITS/32);
-        
-        // modulus might now be even, ensure it's odd
-        instances[index].modulus._limbs[0] |= 1;
+
+        instances[index].modulus._limbs[0] |= 1; // modulus might now be even, ensure it's odd
       }
       else if(compare_words(instances[index].x._limbs, instances[index].modulus._limbs, params::BITS/32)==0) {
-        // since modulus is odd and modulus = x, we can just subtract 1 from x
-        instances[index].x._limbs[0] -= 1;
+        instances[index].x._limbs[0] -= 1; // since modulus is odd and modulus = x, we can just subtract 1 from x
       }
     }
     return instances;
@@ -270,81 +223,65 @@ class powm_odd_t {
   }
 };
 
-// kernel implementation using cgbn
-// 
-// Unfortunately, the kernel must be separate from the powm_odd_t class
-
 template<class params>
 __global__ void kernel_powm_odd(cgbn_error_report_t *report, typename powm_odd_t<params>::instance_t *instances, uint32_t count) {
   int32_t instance;
 
-  // decode an instance number from the blockIdx and threadIdx
   instance=(blockIdx.x*blockDim.x + threadIdx.x)/params::TPI;
   if(instance>=count)
     return;
 
   powm_odd_t<params>                 po(cgbn_report_monitor, report, instance);
   typename powm_odd_t<params>::bn_t  r, x, p, m;
-  
-  // the loads and stores can go in the class, but it seems more natural to have them
-  // here and to pass in and out bignums
+
   cgbn_load(po._env, x, &(instances[instance].x));
   cgbn_load(po._env, p, &(instances[instance].power));
   cgbn_load(po._env, m, &(instances[instance].modulus));
-  
-  // this can be either fixed_window_powm_odd or sliding_window_powm_odd.  
-  // when TPI<32, fixed window runs much faster because it is less divergent, so we use it here
-  po.fixed_window_powm_odd(r, x, p, m);
-  //   OR
-  // po.sliding_window_powm_odd(r, x, p, m);
-  
+
+  po.fixed_window_powm_odd(r, x, p, m); // po.sliding_window_powm_odd(r, x, p, m);
+
   cgbn_store(po._env, &(instances[instance].result), r);
 }
 
 template<class params>
-void run_test(uint32_t instance_count) {
+void run_test(uint32_t nelts, uint32_t addElms) {
   typedef typename powm_odd_t<params>::instance_t instance_t;
 
   instance_t          *instances, *gpuInstances;
   cgbn_error_report_t *report;
   int32_t              TPB=(params::TPB==0) ? 128 : params::TPB;    // default threads per block to 128
   int32_t              TPI=params::TPI, IPB=TPB/TPI;                // IPB is instances per block
-  
+
   printf("Genereating instances ...\n");
   instances=powm_odd_t<params>::generate_instances(instance_count);
-  
+
   printf("Copying instances to the GPU ...\n");
   CUDA_CHECK(cudaSetDevice(0));
   CUDA_CHECK(cudaMalloc((void **)&gpuInstances, sizeof(instance_t)*instance_count));
   CUDA_CHECK(cudaMemcpy(gpuInstances, instances, sizeof(instance_t)*instance_count, cudaMemcpyHostToDevice));
-  
-  // create a cgbn_error_report for CGBN to report back errors
+
   CUDA_CHECK(cgbn_error_report_alloc(&report)); 
-  
+
   printf("Running GPU kernel ...\n");
-  
-  // launch kernel with blocks=ceil(instance_count/IPB) and threads=TPB
+
   kernel_powm_odd<params><<<(instance_count+IPB-1)/IPB, TPB>>>(report, gpuInstances, instance_count);
 
-  // error report uses managed memory, so we sync the device (or stream) and check for cgbn errors
   CUDA_CHECK(cudaDeviceSynchronize());
   CGBN_CHECK(report);
-    
-  // copy the instances back from gpuMemory
+
   printf("Copying results back to CPU ...\n");
   CUDA_CHECK(cudaMemcpy(instances, gpuInstances, sizeof(instance_t)*instance_count, cudaMemcpyDeviceToHost));
-  
+
   printf("Verifying the results ...\n");
   powm_odd_t<params>::verify_results(instances, instance_count);
-  
-  // clean up
+
   free(instances);
   CUDA_CHECK(cudaFree(gpuInstances));
   CUDA_CHECK(cgbn_error_report_free(report));
 }
 
 int main() {
-  typedef powm_params_t<8, 1024, 5> params;
-  
-  run_test<params>(10000);
+  typedef powm_params_t<4, 256, 4> params; // thr/inst = 4, bits = 256, window_bits = 4 (as per fixnum-add BYTES_TO_K_ARY_WINDOW_SIZE_TABLE[])
+
+  run_test<params>(20000, 10000);
 }
